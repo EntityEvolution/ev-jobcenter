96f76288332525cc066ed4f7e33f7d854d9deb5f
XiosBombay <OMGmsebastian@gmail.com>
2021-06-28 15:33:22 -0500

    Feat: Settings page

--- html/js/nui.js
+++ html/js/nui.js
@@ -29,12 +29,53 @@ exitId.addEventListener('click', ()=> {
 background.addEventListener('click', () => {
     background.style.borderBottom = '0.3vh solid #258ef0';
     apps.style.borderBottom = 'none';
+    openTab('background-page', 'settings-page', true);
 });
 
 apps.addEventListener('click', () => {
     background.style.borderBottom = '';
     apps.style.borderBottom = '0.3vh solid #258ef0';
+    openTab('apps-page', 'settings-page', true);
 });
+
+// Apps
+settings.addEventListener('click', () => {
+    openTab('settings', 'tablet-page');
+});
+
+rules.addEventListener('click', () => {
+    openTab('rules', 'tablet-page');
+});
+
+whitelist.addEventListener('click', () => {
+    openTab('whitelist', 'tablet-page');
+});
+
+jobs.addEventListener('click', () => {
+    openTab('jobs', 'tablet-page');
+});
+
+
+function openTab(target, className, settings) {
+    let i, tabcontent;
+    let id = doc.getElementById(target)
+    tabcontent = doc.getElementsByClassName(className);
+    if (settings) {
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.opacity = '0';
+        }
+        id.style.opacity = '1';
+    } else {
+        if (id.style.opacity == '1') {
+            id.style.opacity = '0';
+        } else {
+            for (i = 0; i < tabcontent.length; i++) {
+                tabcontent[i].style.opacity = '0';
+            }
+            id.style.opacity = '1';
+        }
+    }
+}
 
 function fadeAnim(anim, opacity) {
     tablet.style.animation = `${anim} 1s forwards`;
@@ -42,36 +83,6 @@ function fadeAnim(anim, opacity) {
         tablet.style.animation = 'none'
         tablet.style.opacity = `${opacity}`
     }, 600)
-}
-
-settings.addEventListener('click', () => {
-    openTab('settings');
-});
-
-rules.addEventListener('click', () => {
-    openTab('rules');
-});
-
-whitelist.addEventListener('click', () => {
-    openTab('whitelist');
-});
-
-jobs.addEventListener('click', () => {
-    openTab('jobs');
-});
-
-function openTab(target) {
-    let i, tabcontent;
-    let id = doc.getElementById(target)
-    tabcontent = doc.getElementsByClassName('tablet-page');
-    if (id.style.opacity == '1') {
-        id.style.opacity = '0';
-    } else {
-        for (i = 0; i < tabcontent.length; i++) {
-            tabcontent[i].style.opacity = '0';
-        }
-        id.style.opacity = '1';
-    }
 }
 
 const fetchNUI = async(cbname, data)=> {
@@ -85,4 +96,35 @@ const fetchNUI = async(cbname, data)=> {
     const resp = await fetch(`https://ev-jobcenter-esx/${cbname}`, options);
 
     return await resp.json();
+}
+
+
+
+window.addEventListener(`DOMContentLoaded`, () => {
+    fetch(`../html/json/sliders.json`)
+        .then((response) => response.json())
+        .then((data) => {
+            createOptions(data);
+        })
+        .catch((error) => {
+            console.error('Error: ' + error);
+        });
+})
+
+function createOptions(data) {
+	const selection = doc.getElementById('settings-selection');
+    const tabletBack = doc.getElementById('tablet-background')
+    selection.addEventListener(`change`, ()=> {
+        if (selection.value == 'url') {
+            console.log('Custom URL')
+        } else {
+            tabletBack.src = selection.value
+        }
+    })
+	data.forEach(dataItem => {
+		const option = doc.createElement('option');
+		option.text = dataItem.title;
+        option.value = dataItem.background
+		selection.add(option);
+	});
 }
