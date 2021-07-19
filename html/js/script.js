b1869b98eafeb5863e532a9a60dc91da4c018075
XiosBombay <OMGmsebastian@gmail.com>
2021-07-18 19:31:41 -0500

    Feat: Finished rules page and bugs page

--- html/js/script.js
+++ html/js/script.js
@@ -25,18 +25,18 @@ doc.onkeyup = (event) => {
     }
 }
 
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
+const dropdown = document.getElementsByClassName("dropdown-btn");
+let i;
 
 for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var dropdownContent = this.nextElementSibling;
-	console.log(dropdownContent)
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
+  dropdown[i].addEventListener('click', function() {
+    this.classList.toggle('active');
+    const dropdownContent = this.nextElementSibling;
+	  console.log(dropdownContent)
+    if (dropdownContent.style.display === 'block') {
+      dropdownContent.style.display='none';
     } else {
-      dropdownContent.style.display = "block";
+      dropdownContent.style.display='block';
     }
   });
 }
