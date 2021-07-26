ae335edce355d4faeae8960bea57bfe2b8e2ddc3
XiosBombay <OMGmsebastian@gmail.com>
2021-07-26 01:12:20 -0500

    feat(config): Added devMode

--- config/config.js
+++ config/config.js
@@ -1,1 +1,3 @@
 Config = {}
+
+Config.devMode = true
--- html/js/nui.js
+++ html/js/nui.js
@@ -33,11 +33,13 @@ let actBugs = true;
 
 window.addEventListener('load', () => {
     try {
-        console.log('Started jobcenter');
-        fadeAnim('fadeIn', '1');
+        if (Config.devMode) {
+            console.log('Started jobcenter');
+            fadeAnim('fadeIn', '1');
+            doc.getElementById('tablet-background').src = localStorage.getItem('savedBackground')
+        }
         apps.click();
         whitelist.click();
-        // Restore old tablet background //doc.getElementById('tablet-background').src = localStorage.getItem('savedBackground')
     } catch (e) {
         console.log('error: ' + e)
     }
@@ -200,18 +202,19 @@ function openTab(target, className, settings) {
         }
     }
 }
-
-for (let i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener('click', function() {
-    this.classList.toggle('active');
-    const dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === 'block') {
-      dropdownContent.style.display='none';
-    } else {
-      dropdownContent.style.display='block';
+window.addEventListener('load', () => {
+    for (let i = 0; i < dropdown.length; i++) {
+        dropdown[i].addEventListener('click', function() {
+            this.classList.toggle('active');
+            const dropdownContent = this.nextElementSibling;
+            if (dropdownContent.style.display === 'block') {
+                dropdownContent.style.display='none';
+            } else {
+                dropdownContent.style.display='block';
+            }
+        });
     }
-  });
-}
+})
 
 function fadeAnim(anim, opacity) {
     tablet.style.animation = `${anim} 1s forwards`;
